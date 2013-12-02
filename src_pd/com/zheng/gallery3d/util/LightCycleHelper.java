f873a254e8d1343acee21e26344bd105d293afcd
zhengwenhui <zhengwenhui@outlook.com>
2013-12-02 17:30:27 +0800

    clean project

--- src_pd/com/zheng/gallery3d/util/LightCycleHelper.java
+++ src_pd/com/zheng/gallery3d/util/LightCycleHelper.java
@@ -9,60 +9,60 @@ import com.zheng.gallery3d.app.GalleryApp;
 import com.zheng.gallery3d.app.StitchingProgressManager;
 
 public class LightCycleHelper {
-    public static class PanoramaMetadata {
-        // Whether a panorama viewer should be used
-        public final boolean mUsePanoramaViewer;
-        // Whether a panorama is 360 degrees
-        public final boolean mIsPanorama360;
+	public static class PanoramaMetadata {
+		// Whether a panorama viewer should be used
+		public final boolean mUsePanoramaViewer;
+		// Whether a panorama is 360 degrees
+		public final boolean mIsPanorama360;
 
-        public PanoramaMetadata(boolean usePanoramaViewer, boolean isPanorama360) {
-            mUsePanoramaViewer = usePanoramaViewer;
-            mIsPanorama360 = isPanorama360;
-        }
-    }
+		public PanoramaMetadata(boolean usePanoramaViewer, boolean isPanorama360) {
+			mUsePanoramaViewer = usePanoramaViewer;
+			mIsPanorama360 = isPanorama360;
+		}
+	}
 
-    public static class PanoramaViewHelper {
+	public static class PanoramaViewHelper {
 
-        public PanoramaViewHelper(Activity activity) {
-            /* Do nothing */
-        }
+		public PanoramaViewHelper(Activity activity) {
+			/* Do nothing */
+		}
 
-        public void onStart() {
-            /* Do nothing */
-        }
+		public void onStart() {
+			/* Do nothing */
+		}
 
-        public void onCreate() {
-            /* Do nothing */
-        }
+		public void onCreate() {
+			/* Do nothing */
+		}
 
-        public void onStop() {
-            /* Do nothing */
-        }
+		public void onStop() {
+			/* Do nothing */
+		}
 
-        public void showPanorama(Uri uri) {
-            /* Do nothing */
-        }
-    }
+		public void showPanorama(Uri uri) {
+			/* Do nothing */
+		}
+	}
 
-    public static final PanoramaMetadata NOT_PANORAMA = new PanoramaMetadata(false, false);
+	public static final PanoramaMetadata NOT_PANORAMA = new PanoramaMetadata(false, false);
 
-    public static void setupCaptureIntent(Context context, Intent it, String outputDir) {
-        /* Do nothing */
-    }
+	public static void setupCaptureIntent(Context context, Intent it, String outputDir) {
+		/* Do nothing */
+	}
 
-    public static boolean hasLightCycleCapture(Context context) {
-        return false;
-    }
+	public static boolean hasLightCycleCapture(Context context) {
+		return false;
+	}
 
-    public static PanoramaMetadata getPanoramaMetadata(Context context, Uri uri) {
-        return NOT_PANORAMA;
-    }
+	public static PanoramaMetadata getPanoramaMetadata(Context context, Uri uri) {
+		return NOT_PANORAMA;
+	}
 
-/*    public static CameraModule createPanoramaModule() {
+	/*public static CameraModule createPanoramaModule() {
         return null;
     }*/
 
-    public static StitchingProgressManager createStitchingManagerInstance(GalleryApp app) {
-        return null;
-    }
+	public static StitchingProgressManager createStitchingManagerInstance(GalleryApp app) {
+		return null;
+	}
 }
